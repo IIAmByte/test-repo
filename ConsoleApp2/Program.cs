b9d00a962d4e8853b51d58ee81424fad574c459c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 00:32:51 -0400

    Basic user questionarre with World implemented.
    User has a conversation with World specifying their first name, last name and age.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,8 +10,45 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); // Prints out "Hello World!" to console.
+            #region variables
+            
+            string userFirstName;
+            string userLastName;
+            
+            int userAge;
+
+            #endregion
+
+            #region console_output
+
+            // User greeting.
+            Console.WriteLine("User: Hello World!"); // Prints out "Hello World!" to console.
             Console.ReadLine(); // Waits for user input before proceeding, acting effectively as a pause.
+
+            // World response.
+            Console.WriteLine("World: Hello User! What is your first name?");
+
+            // User response, first name.
+            userFirstName = Console.ReadLine();
+
+            // World response to first name.
+            Console.WriteLine("World: {0} is a lovely name! Now, what is your last name?", userFirstName);
+
+            // User response, last name.
+            userLastName = Console.ReadLine();
+
+            // World response to last name.
+            Console.WriteLine("World: {0} is a fantastic last name! Last question, what is your age?", userLastName);
+
+            // User response, age.
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            // World response to age.
+            Console.WriteLine("World: Ah, I remember when I was {0}, that's a great age.", userAge);
+
+            Console.ReadLine(); // Waits for user input before proceeding, acting effectively as a pause.
+            
+            #endregion
         }
     }
 }
