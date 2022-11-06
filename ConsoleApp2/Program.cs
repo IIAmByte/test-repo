e2e263d3f6d40bae73606a1098aa112ec3187a09
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 00:40:51 -0400

    Additional comments for clarification.
    Adds more comments to explain what is happening.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -26,25 +26,25 @@ namespace ConsoleApp2
             Console.ReadLine(); // Waits for user input before proceeding, acting effectively as a pause.
 
             // World response.
-            Console.WriteLine("World: Hello User! What is your first name?");
+            Console.WriteLine("World: Hello User! What is your first name?"); // World asks for User's first name.
 
             // User response, first name.
-            userFirstName = Console.ReadLine();
+            userFirstName = Console.ReadLine(); // Takes input from User and stores it in a var titled userFirstName.
 
             // World response to first name.
-            Console.WriteLine("World: {0} is a lovely name! Now, what is your last name?", userFirstName);
+            Console.WriteLine("World: {0} is a lovely name! Now, what is your last name?", userFirstName); // World responds to User's answer, proceeds to ask for User's last name.
 
             // User response, last name.
-            userLastName = Console.ReadLine();
+            userLastName = Console.ReadLine(); // Takes input from User and stores it in a var titled userLastName.
 
             // World response to last name.
-            Console.WriteLine("World: {0} is a fantastic last name! Last question, what is your age?", userLastName);
+            Console.WriteLine("World: {0} is a fantastic last name! Last question, what is your age?", userLastName); // World responds to User's answer, proceeds to ask for User's age.
 
             // User response, age.
-            userAge = Convert.ToInt32(Console.ReadLine());
+            userAge = Convert.ToInt32(Console.ReadLine()); // Takes input from User and stores it in a var titled userAge.
 
             // World response to age.
-            Console.WriteLine("World: Ah, I remember when I was {0}, that's a great age.", userAge);
+            Console.WriteLine("World: Ah, I remember when I was {0}, that's a great age.", userAge); // World responds to User's answer.
 
             Console.ReadLine(); // Waits for user input before proceeding, acting effectively as a pause.
             
